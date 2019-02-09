70493ff7e9c41e99babf98895ac7f263bf19a8d6
Phani <phani2585@gmail.com>
2019-02-09 13:52:32 +0530

    Admin Controller committed

---
+++ quora_stub/Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/UserAdminBusinessService.java
@@ -0,0 +1,37 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+    public class UserAdminBusinessService {
+
+        @Autowired
+        private UserDao userDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+        public UserEntity getUser(final String userUuid,final String authorizationToken) throws
+            UserNotFoundException, AuthorizationFailedException {
+        UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthToken(authorizationToken);
+        if(userAuthTokenEntity == null){
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        } else if(userAuthTokenEntity.getLogoutAt()!=null){
+            throw new AuthorizationFailedException("ATHR-002","User is signed out");
+        } else if (userDao.getUserByUuid(userUuid)== null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
+        } else {
+            return userDao.getUserByUuid(userAuthTokenEntity.getUuid());
+        }
+    }
+}
+
+
+
+
